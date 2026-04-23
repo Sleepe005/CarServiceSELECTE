7a8bb3bab72a602cd8ff38bc7e5aaf5de34737e1
matt <mariiamarina70@gmail.com>
2026-04-23 14:48:27 +0300

    Add files via upload

--- CarAPI_1/src/main/java/com/example/CarAPI_1/model/Cars.java
+++ CarAPI_1/src/main/java/com/example/CarAPI_1/model/Cars.java
@@ -4,21 +4,24 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.Setter;
-import java.time.LocalDate;
 
 @Getter
-@Setter(value = AccessLevel.PROTECTED)
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "cars")
+@Table(name = "Cars")
 public class Cars {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id", nullable = false)
+    //@Setter(AccessLevel.PROTECTED)
     private Long id;
 
+    //минимальные тех критерии для вывода
     @Column(name="brand", nullable = false)
     private String brand;
 
@@ -37,33 +40,6 @@ public class Cars {
     @Column(name="price", nullable = false)
     private Long price;
 
-    @Column(name="safety_rating", nullable = false)
-    private Long safety_rating;
-
-    @Column(name="reliability_rating", nullable = false)
-    private Long reliability_rating;
-
-    @Column(name="fuel_consumption", nullable = false)
-    private Long fuel_consumption;
-
-    @Column(name="maintenance_cost", nullable = false)
-    private Long maintenance_cost;
-
-    @Column(name="comfort_rating", nullable = false)
-    private Long comfort_rating;
-
-    @Column(name="capacity_passengers", nullable = false)
-    private Long capacity_passengers;
-
-    @Column(name="capacity_baggage", nullable = false)
-    private Long capacity_baggage;
-
-    @Column(name="acceleration_0_100", nullable = false)
-    private Long acceleration_0_100;
-
-    @Column(name="appearance_rating", nullable = false)
-    private Long appearance_rating;
-
     @Column(name="body_type", nullable = false)
     private String body_type;
 
@@ -79,40 +55,33 @@ public class Cars {
     @Column(name="power_hp", nullable = false)
     private Long power_hp;
 
-    @Column(name="additional_options", nullable = false)
-    private String additional_options;
 
-    @Column(name="last_updated", nullable = false)
-    private java.time.LocalDate last_updated;
 
-    public Cars(String brand, String model, String generation,
-                LocalDate year_from, LocalDate year_to, Long price, Long safety_rating,
-                Long reliability_rating, Long fuel_consumption, Long maintenance_cost,
-                Long comfort_rating, Long capacity_passengers, Long capacity_baggage, Long acceleration_0_100,
-                Long appearance_rating, String body_type, String fuel_type, String transmission,
-                String drivetrain, Long power_hp, String additional_options, LocalDate last_updated) {
-        this.brand = brand;
-        this.model = model;
-        this.generation = generation;
-        this.year_from = year_from;
-        this.year_to = year_to;
-        this.price = price;
-        this.safety_rating = safety_rating;
-        this.reliability_rating = reliability_rating;
-        this.fuel_consumption = fuel_consumption;
-        this.maintenance_cost = maintenance_cost;
-        this.comfort_rating = comfort_rating;
-        this.capacity_passengers = capacity_passengers;
-        this.capacity_baggage = capacity_baggage;
-        this.acceleration_0_100 = acceleration_0_100;
-        this.appearance_rating = appearance_rating;
-        this.body_type = body_type;
-        this.fuel_type = fuel_type;
-        this.transmission = transmission;
-        this.drivetrain = drivetrain;
-        this.power_hp = power_hp;
-        this.additional_options = additional_options;
-        this.last_updated = last_updated;
-    }
+
+    //пользовательские критерии для electre
+    @Column(name="safety_rating", nullable = false)
+    private Long safety_rating;
+
+    @Column(name="reliability_rating", nullable = false)
+    private Long reliability_rating;
+
+    @Column(name="economy_rating", nullable = false)
+    private Long economy_rating;
+
+    @Column(name="comfort_rating", nullable = false)
+    private Long comfort_rating;
+
+    @Column(name="capacity_rating", nullable = false)
+    private Long capacity_rating;
+
+    @Column(name="dynamics_rating", nullable = false)
+    private Long dynamics_rating;
+
+    @Column(name="appearance_rating", nullable = false)
+    private Long appearance_rating;
+
+    @Column(name="features_rating", nullable = false)
+    private Long features_rating;
+
 
 }
